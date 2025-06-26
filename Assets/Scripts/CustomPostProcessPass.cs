56eb9408b71b52329884c2939c6f67a9140a4c11
Bork114 <mattisderyckere@gmail.com>
2025-06-26 21:31:22 +0200

    Add Custom Render Pass Scripts

---
+++ Assets/Scripts/CustomPostProcessPass.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+[System.Serializable]
+public class CustomPostProcessPass : ScriptableRendererPass
+{
+    public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData){
+
+    }
+}
