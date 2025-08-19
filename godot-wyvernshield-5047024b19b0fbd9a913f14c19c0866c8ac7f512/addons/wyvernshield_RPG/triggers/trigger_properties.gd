d9af8c5f8c0fb74e10325c3b51ca74ed3c04178e
Bork114 <mattisderyckere@gmail.com>
2025-08-19 12:52:49 +0200

    delete sub project

--- godot-wyvernshield-5047024b19b0fbd9a913f14c19c0866c8ac7f512/addons/wyvernshield_RPG/triggers/trigger_properties.gd
+++
@@ -1,5 +0,0 @@
-class_name TriggerProperties
-extends Resource
-
-export var trigger_name := "tick"
-export(String, MULTILINE) var prop_names
--- godot-wyvernshield-5047024b19b0fbd9a913f14c19c0866c8ac7f512/addons/wyvernshield_RPG/triggers/trigger_reaction_instance.gd
+++
@@ -1,7 +0,0 @@
-class_name TriggerReactionInstance
-extends Resource
-
-var apply_function : FuncRef
-var priority : int
-var extra_vars : Array
-var source_status_effect : StatusEffect
