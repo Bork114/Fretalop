cc93df67ba47f7d0e41e3e3235289c24c7ce3947
Bork114 <mattisderyckere@gmail.com>
2025-08-19 12:52:04 +0200

    Delete sub project

---
+++ Scenes/player.gd
@@ -0,0 +1,16 @@
+extends CharacterBody3D
+
+@export_group("Group")
+@export_range(0.0,1.0) var mouse_sensitivity := 0.25
+
+var _camera_input_direction := Vector2.ZERO
+
+@onready var _camera_pivot : Node3D =  %CameraPivot
+
+func _unhandled_input(event: InputEvent) -> void:
+	var is_camera_motion := (
+		event is InputEventMouseMotion and 
+		Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED
+	)
+	if is_camera_motion:
+		_camera_input_direction = event.screen_relative * mouse_sensitivity
